45413f308a3d319d6baa9241759481f60ef611cd
= <=>
2024-03-14 11:26:14 +0100

    Labguide V2.6

--- SMEXHybrid/Scripts/PrepareStudentCourseInfoFiles.ps1
+++ SMEXHybrid/Scripts/PrepareStudentCourseInfoFiles.ps1
@@ -1,8 +1,8 @@
 ﻿[cmdletbinding()]
 
 Param(
-[Parameter(Mandatory = $true)]
-[System.IO.FileInfo]$Path
+    [Parameter(Mandatory = $true)]
+    [System.IO.FileInfo]$Path
 )
 
 # Import Excel Module
@@ -18,31 +18,27 @@ Else
 }
 
 # Import Excel File
-$x = Import-Excel -Path $Path.FullName -StartRow 3 -StartColumn 1 -EndColumn 11  -NoHeader
+$x = Import-Excel -Path $Path.FullName -StartRow 3 -StartColumn 1 -EndColumn 12 -NoHeader
 
 foreach ($entry in $x)
 {
-    $Filename = $entry.P1.Replace(" ","") + ".txt"
+    $Filename = $entry.P2.Replace(" ", "_") + ".txt"
     $OutputFile = Join-Path -Path $Path.Directory -ChildPath (Join-Path -Path "Kursinfo Dateien" -ChildPath $Filename)
 
     if (-not (Test-Path $OutputFile -PathType Leaf))
-        {
-            New-Item -ItemType File -Path $OutputFile -Force -Confirm:$false -WhatIf:$false | Out-Null
-        }
+    {
+        New-Item -ItemType File -Path $OutputFile -Force -Confirm:$false -WhatIf:$false | Out-Null
+    }
     
-    $TNDomain = $entry.p3+$entry.p4
-    $Customdomain = $TNDomain + ".myetc.at"
-    $Tenantname = $entry.p6.replace(" ","") + $entry.p7
-    $Tenantadmin = "admin@" + $Tenantname
-    Add-Content -Value "Teilnehmer-Domain: $TNDomain" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Custom-Domain: $Customdomain`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Tenantname: $Tenantname" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Tenantadmin: $Tenantadmin`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "IP Netz: 78.142.168.129" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Subnet Mask: 255.255.255.192" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Gateway: 78.142.168.129`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "IP1: 78.142.168.$($entry.p8)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "IP2: 78.142.168.$($entry.p9)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Azure DNS User: $($entry.p10)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
-    Add-Content -Value "Azure DNS User PW: $($entry.p11)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Teilnehmer-Domain:`t$($entry.p4)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Custom-Domain:`t`t$($entry.p5)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantdomain:`t`t$($entry.p6)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantadmin:`t`t$($entry.p7)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantadmin PW:`t`t$($entry.p8)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Azure DNS User:`t`t$($entry.p11)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Azure DNS User PW:`t$($entry.p12)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "IP1:`t`t`t$($entry.p9)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "IP2:`t`t`t$($entry.p10)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Subnet Mask:`t`t255.255.255.192" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Gateway:`t`t78.142.168.129" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
 }

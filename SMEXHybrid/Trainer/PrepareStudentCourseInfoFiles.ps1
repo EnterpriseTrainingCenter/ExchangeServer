36096ceb587695dcbe64b96a8544edc1dc5d79a0
Christian Schindler <cnschindler@gmx.net>
2025-11-02 09:49:39 +0100

    LAB Gide V 3.2
    Add PowerShell script to prepare student course info files
    
    - Created a new script `PrepareStudentCourseInfoFiles.ps1` to automate the generation of course information files for students.
    - The script imports data from an Excel file and generates individual text files for each student with relevant information.
    - Added error handling for missing ImportExcel module and ensured the script only processes the specified number of students.
    - Each output file includes participant domain, custom domain, tenant domain, tenant admin, and other necessary details.

---
+++ SMEXHybrid/Trainer/PrepareStudentCourseInfoFiles.ps1
@@ -0,0 +1,46 @@
+﻿[cmdletbinding()]
+
+Param(
+    [Parameter(Mandatory = $true)]
+    [System.IO.FileInfo]$Path,
+    [Parameter(Mandatory = $true)]
+    [int]$NumberofStudents
+)
+
+# Import Excel Module
+if (Get-Module -ListAvailable ImportExcel)
+{
+    Import-Module ImportExcel
+}
+
+Else
+{
+    Write-Host -ForegroundColor Yellow "Module ImportExcel not found. Please install this module!"
+    Exit
+}
+
+# Import Excel File
+$x = Import-Excel -Path $Path.FullName -StartRow 5 -StartColumn 1 -EndColumn 11 -EndRow (4 + $NumberofStudents) -NoHeader
+
+foreach ($entry in $x)
+{
+    $Filename = $entry.P2.ToString() + ".txt"
+    $OutputFile = Join-Path -Path $Path.Directory -ChildPath (Join-Path -Path "Kursinfo Dateien" -ChildPath $Filename)
+
+    if (-not (Test-Path $OutputFile -PathType Leaf))
+    {
+        New-Item -ItemType File -Path $OutputFile -Force -Confirm:$false -WhatIf:$false | Out-Null
+    }
+
+    Add-Content -Value "Teilnehmer-Domain:`t$($entry.p3)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Custom-Domain:`t`t$($entry.p3)$($entry.P4)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantdomain:`t`t$($entry.p5)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantadmin:`t`t$($entry.p6)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Tenantadmin PW:`t`t$($entry.p7)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Azure DNS User:`t`t$($entry.P10)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Azure DNS User PW:`t$($entry.P11)`n" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "IP1:`t`t`t$($entry.P8)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "IP2:`t`t`t$($entry.P9)" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Subnet Mask:`t255.255.255.192" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+    Add-Content -Value "Gateway:`t`t78.142.168.129" -Path $OutputFile -Encoding UTF8 -WhatIf:$false -Confirm:$false
+}
